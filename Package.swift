fb44a854de2a8eeb18ebef503d1db2bc97bf77ec
Jonathan Wight <jwight@mac.com>
2022-09-24 16:00:32 -0700

    Bumped versions back up due to Viewer.
    Main module is safe on current - 1. But we use parts of SwiftUI in the viewer. Break view into own app as a solutin.

--- Package.swift
+++ Package.swift
@@ -6,16 +6,15 @@ import PackageDescription
 let package = Package(
     name: "SwiftGLTF",
     platforms: [
-        .iOS("15.0"),
-        .macOS("12.0"),
-        .macCatalyst("15.0")
+        .iOS("16.0"),
+        .macOS("13.0"),
+        .macCatalyst("16.0")
     ],
     products: [
         .library(
             name: "SwiftGLTF",
             targets: ["SwiftGLTF"]
         ),
-        .executable(name: "SwiftGLTFViewer", targets: ["SwiftGLTFViewer"]),
     ],
     dependencies: [
         .package(url: "https://github.com/schwa/Everything", branch: "main"),
